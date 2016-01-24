5b40646ad7d7ae740035174dd07f70e86ad6a64f
ashmna <mnatsakanyan.ashot@gmail.com>
2016-01-24 19:14:36 +0400

    Added completed property in root scope

--- ng/controllers/RootController.js
+++ ng/controllers/RootController.js
@@ -1,7 +1,18 @@
 'use strict';
 
-app.controller("RootController", ['$rootScope', RootController]);
-
-function RootController($rootScope) {
+function RootController($rootScope, UserService) {
+  $rootScope.completed = false;
   $rootScope.title = "";
+  $rootScope.userData = null;
+
+  UserService.subscribe('get-user-data.success', function(data){
+    $rootScope.userData = data;
+    $rootScope.completed = true;
+  });
+
+  UserService.getUserData();
 }
+
+// register controller
+RootController.$inject = ['$rootScope', 'UserService'];
+app.controller('RootController', RootController);
