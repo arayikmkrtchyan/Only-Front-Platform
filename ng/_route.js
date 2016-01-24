ec0b1aeeed217bb13bea43805a03cad7d3f738d0
ashmna <mnatsakanyan.ashot@gmail.com>
2016-01-24 19:18:18 +0400

    Added common pages in main route

--- ng/_route.js
+++
@@ -1,9 +0,0 @@
-app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
-
-
-  $routeProvider.when('/demo', {templateUrl: 'templates/demo.html', controller: 'DemoController'});
-
-
-  //$locationProvider.html5Mode(true);
-
-}]);
