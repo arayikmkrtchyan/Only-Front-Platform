8c89293fd3d906bfc06ec4f51399a37f1a3fe5b2
ashmna <mnatsakanyan.ashot@gmail.com>
2016-01-24 19:14:02 +0400

    Added demo routes with roles

--- projects/demo/route.js
+++ projects/demo/route.js
@@ -1,9 +1,8 @@
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
 
+  $routeProvider.when('/demo1', {templateUrl: 'templates/demo.html', controller: 'DemoController', roles: ['Admin']});
+  $routeProvider.when('/demo2', {templateUrl: 'templates/demo.html', controller: 'DemoController', roles: ['Admin']});
+  $routeProvider.when('/demo3', {templateUrl: 'templates/demo.html', controller: 'DemoController', roles: ['Admin']});
 
-  $routeProvider.when('/demo', {templateUrl: 'templates/demo.html', controller: 'DemoController'});
-
-
-  //$locationProvider.html5Mode(true);
 
 }]);
