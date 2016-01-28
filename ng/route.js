df0596ccbfff9a115f717b0ba76b688df18ad8a9
ashmna <mnatsakanyan.ashot@gmail.com>
2016-01-28 22:48:21 +0400

    Deleted because is deprecated

--- ng/route.js
+++
@@ -1,7 +0,0 @@
-app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
-
-  $routeProvider.when('/sign-in',       {templateUrl: 'templates/sign-in.html',       controller: 'LoginController', roles: ['Guest']});
-  $routeProvider.when('/sign-up',       {templateUrl: 'templates/sign-up.html',       controller: 'LoginController', roles: ['Guest']});
-  $routeProvider.when('/access-denied', {templateUrl: 'templates/access-denied.html' });
-
-}]);
