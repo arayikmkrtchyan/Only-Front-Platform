d99ea675f86d03f75ff2725ec5ac43e6a7dd4501
ashmna <mnatsakanyan.ashot@gmail.com>
2016-01-28 22:46:31 +0400

    Demo project files from presentation

--- projects/demo/route.js
+++
@@ -1,8 +0,0 @@
-app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
-
-  $routeProvider.when('/demo1', {templateUrl: 'templates/demo.html', controller: 'DemoController', roles: ['Admin']});
-  $routeProvider.when('/demo2', {templateUrl: 'templates/demo.html', controller: 'DemoController', roles: ['Admin']});
-  $routeProvider.when('/demo3', {templateUrl: 'templates/demo.html', controller: 'DemoController', roles: ['Admin']});
-
-
-}]);
