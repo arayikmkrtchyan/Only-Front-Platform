cbbb7f753df7a049756746e285fdada24361366f
ashmna <mnatsakanyan.ashot@gmail.com>
2016-02-03 01:05:26 +0400

    Added gulp-sequence into dev dependency list and modify gulp tasks using that

--- gulpfile.js
+++ gulpfile.js
@@ -1,140 +1,136 @@
-'use strict';
+"use strict";
 
-var gulp     = require('gulp'),
-  config     = require('./config'),
-  del        = require('del'),
-  concat     = require('gulp-concat'),
-  uglify     = require('gulp-uglify'),
+var gulp     = require("gulp"),
+  config     = require("./config"),
+  del        = require("del"),
+  concat     = require("gulp-concat"),
+  uglify     = require("gulp-uglify"),
   rename     = require("gulp-rename"),
-  concatCss  = require('gulp-concat-css'),
-  cssnano    = require('gulp-cssnano'),
-  htmlmin    = require('gulp-htmlmin'),
-  preprocess = require('gulp-preprocess'),
-  inject     = require('gulp-inject');
+  concatCss  = require("gulp-concat-css"),
+  cssnano    = require("gulp-cssnano"),
+  htmlmin    = require("gulp-htmlmin"),
+  preprocess = require("gulp-preprocess"),
+  inject     = require("gulp-inject"),
+  sequence   = require("gulp-sequence");
 
-// Implements job
-var
-/** JS app concat and uglify */
-job_app_js = function () {
-  gulp.src(config.src.jsFiles.concat(config.project.jsFiles))
-    .pipe(concat('app.js'))
+// Sub task list
+gulp.task("clear-public", function(){
+  del(config.public.root);
+});
+
+gulp.task("build-app-js", function(){
+  return gulp.src(config.src.jsFiles.concat(config.project.jsFiles))
+    .pipe(concat("app.js"))
     .pipe(gulp.dest(config.public.js))
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
+    .pipe(rename({ extname: ".min.js" }))
     .pipe(gulp.dest(config.public.js))
   ;
-},
+});
 
-/** JS vendor concat and uglify */
-job_vendor_js = function () {
-  gulp.src(config.src.jsVendor.concat(config.project.jsVendor))
-    .pipe(concat('vendor.min.js'))
+gulp.task("build-vendor-js", function(){
+  return gulp.src(config.src.jsVendor.concat(config.project.jsVendor))
+    .pipe(concat("vendor.min.js"))
     .pipe(gulp.dest(config.public.js))
   ;
-},
+});
 
-/** CSS style concat ans minify */
-job_style_css = function() {
-  gulp.src([config.src.cssFiles, config.project.cssFiles])
-    .pipe(concatCss('style.css'))
+gulp.task("build-style-css", function(){
+  return gulp.src([config.src.cssFiles, config.project.cssFiles])
+    .pipe(concatCss("style.css"))
     .pipe(gulp.dest(config.public.css))
     .pipe(cssnano())
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(rename({ extname: ".min.css" }))
     .pipe(gulp.dest(config.public.css))
   ;
-},
+});
 
-/** CSS vendor concat ans minify */
-job_vendor_css = function() {
-  gulp.src(config.src.cssVendor.concat(config.project.cssVendor))
-    .pipe(concatCss('vendor.css'))
+gulp.task("build-vendor-css", function(){
+  return gulp.src(config.src.cssVendor.concat(config.project.cssVendor))
+    .pipe(concatCss("vendor.css"))
     .pipe(gulp.dest(config.public.css))
     .pipe(cssnano())
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(rename({ extname: ".min.css" }))
     .pipe(gulp.dest(config.public.css))
   ;
-},
+});
 
-/** Images copy to public */
-job_images = function() {
-  gulp.src([config.src.imgFiles, config.project.imgFiles])
+gulp.task("copy-images", function(){
+  return gulp.src([config.src.imgFiles, config.project.imgFiles])
     .pipe(gulp.dest(config.public.img))
   ;
-},
+});
 
-/** HTML copy to public and compress */
-job_html = function() {
-  gulp.src([config.src.htmlFiles, config.project.htmlFiles])
+gulp.task("build-html", function(){
+  return gulp.src([config.src.htmlFiles, config.project.htmlFiles])
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest(config.public.html))
   ;
-},
+});
 
-/** HTML index copy to public and compress */
-job_html_index = function() {
-
+gulp.task("build-index", function(){
   var target = gulp.src(config.src.htmlIndexFile);
 
   if(config.context.DEBUG) {
     var files = [];
     files = files.concat(config.src.jsFiles);
     files = files.concat(config.project.jsFiles);
-    files.push(config.src.cssFiles);
-    files.push(config.project.cssFiles);
-    var sources = gulp.src(files, {read: false});
-    target.pipe(inject(sources));
+    //files.push(config.src.cssFiles);
+    //files.push(config.project.cssFiles);
+    var sources = gulp.src(files);
+    target.pipe(inject(sources, {relative: true}));
   } else {
     target.pipe(htmlmin({collapseWhitespace: true}))
   }
-  target.pipe(preprocess({context : config.context}));
-  target.pipe(gulp.dest(config.public.root));
-},
+  //target.pipe(preprocess({context : config.context}));
+  return target.pipe(gulp.dest(config.public.root));
+});
 
-/** REST json files copy to public rest folder */
-job_rest = function() {
-  gulp.src([config.src.restFiles, config.project.restFiles])
-    .pipe(htmlmin({collapseWhitespace: true}))
+gulp.task("build-rest", function(){
+  return gulp.src([config.src.restFiles, config.project.restFiles])
     .pipe(gulp.dest(config.public.rest))
   ;
-};
-// Implements job !end
+});
 
 
 // Register gulp tasks
 
-gulp.task('deploy', function () {
+gulp.task("deploy", function (cb) {
   config.context.DEBUG = false;
-  config.context.ENV = 'production';
+  config.context.ENV = "production";
 
-  // delete all public dir
-  del(config.public.root);
-
-  job_app_js();
-  job_vendor_js();
-  job_style_css();
-  job_vendor_css();
-  job_images();
-  job_html();
-  job_html_index();
-  job_rest();
-
-});
-
-gulp.task('watch', function () {
-
-  gulp.watch(config.src.jsFiles.concat(config.project.jsFiles),        job_app_js     );
-  gulp.watch([config.src.cssFiles, config.project.cssFiles],           job_style_css  );
-  gulp.watch([config.src.imgFiles, config.project.imgFiles],           job_images     );
-  gulp.watch([config.src.htmlFiles, config.project.htmlFiles],         job_html       );
-  gulp.watch([config.src.htmlIndexFile, config.project.htmlIndexFile], job_html_index );
-  gulp.watch([config.src.restFiles, config.project.restFiles],         job_rest       );
+  sequence(
+    "clear-public",
+    [
+      "build-app-js",
+      "build-vendor-js",
+      "build-style-css",
+      "build-vendor-css",
+      "copy-images",
+      "build-html",
+      "build-index",
+      "build-rest"
+    ]
+  )(cb);
 
 });
 
-gulp.task('develop', function () {
+gulp.task("watch", function () {
+
+  gulp.watch(config.src.jsFiles.concat(config.project.jsFiles),        "build-app-js");
+  gulp.watch([config.src.cssFiles, config.project.cssFiles],           "build-style-css");
+  gulp.watch([config.src.imgFiles, config.project.imgFiles],           "copy-images");
+  gulp.watch([config.src.htmlFiles, config.project.htmlFiles],         "build-html");
+  gulp.watch([config.src.htmlIndexFile, config.project.htmlIndexFile], "build-index");
+  gulp.watch([config.src.restFiles, config.project.restFiles],         "build-rest");
+
+});
+
+gulp.task("develop", function () {
   config.context.DEBUG = true;
-  config.context.ENV = 'development';
-  job_html_index();
+  config.context.ENV = "development";
+
+  gulp.run("build-index");
 
   var files = [];
   files = files.concat(config.src.jsFiles);
@@ -143,6 +139,6 @@ gulp.task('develop', function () {
   files.push(config.project.cssFiles);
   files.push(config.src.htmlIndexFile);
   files.push(config.project.htmlIndexFile);
-  return gulp.watch(files, job_html_index);
+  return gulp.watch(files, "build-index");
 
 });
