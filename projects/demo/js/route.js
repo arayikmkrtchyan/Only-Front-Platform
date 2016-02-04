4226b0bd80d314d756a61818eb3133f0e1e786d4
ashmna <mnatsakanyan.ashot@gmail.com>
2016-02-05 03:20:25 +0400

    Added error 404 page into routes

--- projects/demo/js/route.js
+++ projects/demo/js/route.js
@@ -1,7 +1,7 @@
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
 
-  $routeProvider.when('/demo1', {templateUrl: '../html/demo.html', controller: 'DemoController', roles: ['Admin']});
-  $routeProvider.when('/demo2', {templateUrl: '../html/demo.html', controller: 'DemoController', roles: ['Admin']});
-  $routeProvider.when('/demo3', {templateUrl: '../html/demo.html', controller: 'DemoController', roles: ['Admin']});
+  $routeProvider.when('/demo1', {templateUrl: 'html/demo.html', controller: 'DemoController', roles: ['Admin']});
+  $routeProvider.when('/demo2', {templateUrl: 'html/demo.html', controller: 'DemoController', roles: ['Admin']});
+  $routeProvider.when('/demo3', {templateUrl: 'html/demo.html', controller: 'DemoController', roles: ['Admin']});
 
 }]);
