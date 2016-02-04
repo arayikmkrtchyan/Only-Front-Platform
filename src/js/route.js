1d9706dcc17bb340cc498a1e1745a4de9c8946c2
ashmna <mnatsakanyan.ashot@gmail.com>
2016-02-05 03:21:05 +0400

    Fixed html file paths

--- src/js/route.js
+++ src/js/route.js
@@ -1,8 +1,9 @@
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
 
-  $routeProvider.when('/sign-in',       {templateUrl: '../templates/sign-in.html',       controller: 'LoginController', roles: ['Guest']});
-  $routeProvider.when('/sign-up',       {templateUrl: '../templates/sign-up.html',       controller: 'LoginController', roles: ['Guest']});
-  $routeProvider.when('/access-denied', {templateUrl: '../templates/access-denied.html' });
+  $routeProvider.when('/sign-in',       {templateUrl: 'html/sign-in.html',       controller: 'LoginController', roles: ['Guest']});
+  $routeProvider.when('/sign-up',       {templateUrl: 'html/sign-up.html',       controller: 'LoginController', roles: ['Guest']});
+  $routeProvider.when('/access-denied', {templateUrl: 'html/access-denied.html' });
+  $routeProvider.when('/error-404', {templateUrl: 'html/error/404.html' });
 
 }]);
 
