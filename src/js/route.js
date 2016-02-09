9754776eb5e8cbd745582e6c50ec8bc725e3535c
ashmna <mnatsakanyan.ashot@gmail.com>
2016-02-09 21:27:08 +0400

    access denied route bug fixed

--- src/js/route.js
+++ src/js/route.js
@@ -1,7 +1,7 @@
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
 
   Route.addToRoutesList("sign-in",       Route.ng("sign-in.html"    , "LoginController", ["Guest"]));
-  Route.addToRoutesList("access-denied", Route.ng("sign-denied.html", "", []));
+  Route.addToRoutesList("access-denied", Route.ng("access-denied.html", "", []));
   Route.addToRoutesList("error-404",     Route.ng("error/404.html"  , "", []));
 
   $routeProvider.when("/sign-in",       Route.getFromRoutesList("sign-in") );
