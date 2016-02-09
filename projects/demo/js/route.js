298d9fb93aa6a1dac6d159f35d8516a2f49adc86
ashmna <mnatsakanyan.ashot@gmail.com>
2016-02-09 21:28:44 +0400

    Change routes using Rout ng method

--- projects/demo/js/route.js
+++ projects/demo/js/route.js
@@ -1,7 +1,7 @@
 app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
 
-  $routeProvider.when('/demo1', {templateUrl: 'html/demo.html', controller: 'DemoController', roles: ['Admin']});
-  $routeProvider.when('/demo2', {templateUrl: 'html/demo.html', controller: 'DemoController', roles: ['Admin']});
-  $routeProvider.when('/demo3', {templateUrl: 'html/demo.html', controller: 'DemoController', roles: ['Admin']});
+  $routeProvider.when("/demo1", Route.ng("demo.html", "DemoController1", ["Admin"]));
+  $routeProvider.when("/demo2", Route.ng("demo.html", "DemoController2", ["Admin"]));
+  $routeProvider.when("/demo3", Route.ng("demo.html", "DemoController3", ["Admin"]));
 
 }]);
