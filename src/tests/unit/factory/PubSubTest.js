141e7af483b64550d2a0fd70f04bbbf08e510fc9
ashmna <mnatsakanyan.ashot@gmail.com>
2016-02-14 12:57:42 +0400

    Improve code style and move into Factory describe

--- src/tests/unit/factory/PubSubTest.js
+++ src/tests/unit/factory/PubSubTest.js
@@ -1,87 +1,91 @@
-describe("PubSub", function() {
+describe("Factory", function () {
 
-  it("test with subscribe and publish", function() {
-    // Test data
-    var pubSub = new PubSub();
-    var counter = 0;
-    var eventName = 'some-event';
-    var data = {};
-    // Run test
-    pubSub.subscribe(eventName, function(eventData) {
-      expect(eventData).toEqual(data);
-      ++counter;
-    });
-    var resTrue = pubSub.publish(eventName, data);
-    var resFalse = pubSub.publish('otherEvent', {});
-    expect(resTrue).toBe(true);
-    expect(resFalse).toBe(false);
-    expect(counter).toEqual(1);
-  });
+  describe("Testing 'PubSub'", function () {
 
-  it("test with many subscribe", function() {
-    // Test data
-    var pubSub = new PubSub();
-    var counter = 0;
-    var eventName = 'some-event';
-    // Run test
-    pubSub.subscribe(eventName, function() {
-      ++counter;
+    it("test with subscribe and publish", function () {
+      // Test data
+      var pubSub = new PubSub();
+      var counter = 0;
+      var eventName = 'some-event';
+      var data = {};
+      // Run test
+      pubSub.subscribe(eventName, function (eventData) {
+        expect(eventData).toEqual(data);
+        ++counter;
+      });
+      var resTrue = pubSub.publish(eventName, data);
+      var resFalse = pubSub.publish('otherEvent', {});
+      expect(resTrue).toBe(true);
+      expect(resFalse).toBe(false);
+      expect(counter).toEqual(1);
     });
-    pubSub.subscribe(eventName, function() {
-      ++counter;
-    });
-    pubSub.subscribe(eventName, function() {
-      ++counter;
-    });
-    pubSub.publish(eventName, {});
-    expect(counter).toEqual(3);
-  });
 
-  it("test inherited object with subscribe and publish", function() {
-    // Test data
-    function SomeObject($http) {
-    }
+    it("test with many subscribe", function () {
+      // Test data
+      var pubSub = new PubSub();
+      var counter = 0;
+      var eventName = 'some-event';
+      // Run test
+      pubSub.subscribe(eventName, function () {
+        ++counter;
+      });
+      pubSub.subscribe(eventName, function () {
+        ++counter;
+      });
+      pubSub.subscribe(eventName, function () {
+        ++counter;
+      });
+      pubSub.publish(eventName, {});
+      expect(counter).toEqual(3);
+    });
 
-    SomeObject.prototype = Object.create(PubSub.prototype);
-    SomeObject.prototype.constructor = SomeObject;
-    var someObject = new SomeObject();
-    var counter = 0;
-    var eventName = 'some-event';
-    var data = {};
-    // Run test
-    someObject.subscribe(eventName, function(eventData) {
-      expect(eventData).toEqual(data);
-      ++counter;
-    });
-    var resTrue = someObject.publish(eventName, data);
-    var resFalse = someObject.publish('otherEvent', {});
-    expect(resTrue).toBe(true);
-    expect(resFalse).toBe(false);
-    expect(counter).toEqual(1);
-  });
+    it("test inherited object with subscribe and publish", function () {
+      // Test data
+      function SomeObject($http) {
+      }
 
-  it("test inherited object with many subscribe", function() {
-    // Test data
-    function SomeObject($http) {
-    }
+      SomeObject.prototype = Object.create(PubSub.prototype);
+      SomeObject.prototype.constructor = SomeObject;
+      var someObject = new SomeObject();
+      var counter = 0;
+      var eventName = 'some-event';
+      var data = {};
+      // Run test
+      someObject.subscribe(eventName, function (eventData) {
+        expect(eventData).toEqual(data);
+        ++counter;
+      });
+      var resTrue = someObject.publish(eventName, data);
+      var resFalse = someObject.publish('otherEvent', {});
+      expect(resTrue).toBe(true);
+      expect(resFalse).toBe(false);
+      expect(counter).toEqual(1);
+    });
 
-    SomeObject.prototype = Object.create(PubSub.prototype);
-    SomeObject.prototype.constructor = SomeObject;
-    var someObject = new SomeObject();
-    var counter = 0;
-    var eventName = 'some-event';
-    // Run test
-    someObject.subscribe(eventName, function() {
-      ++counter;
+    it("test inherited object with many subscribe", function () {
+      // Test data
+      function SomeObject($http) {
+      }
+
+      SomeObject.prototype = Object.create(PubSub.prototype);
+      SomeObject.prototype.constructor = SomeObject;
+      var someObject = new SomeObject();
+      var counter = 0;
+      var eventName = 'some-event';
+      // Run test
+      someObject.subscribe(eventName, function () {
+        ++counter;
+      });
+      someObject.subscribe(eventName, function () {
+        ++counter;
+      });
+      someObject.subscribe(eventName, function () {
+        ++counter;
+      });
+      someObject.publish(eventName, {});
+      expect(counter).toEqual(3);
     });
-    someObject.subscribe(eventName, function() {
-      ++counter;
-    });
-    someObject.subscribe(eventName, function() {
-      ++counter;
-    });
-    someObject.publish(eventName, {});
-    expect(counter).toEqual(3);
+
   });
 
 });
